39b0cb557fba65cd0e719f3db197c2792ef2f6b7
Brian Harrington <brharrington@gmail.com>
2018-03-14 21:21:49 -0700

    additional tests for counters

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -12,3 +12,16 @@ class CounterTest(unittest.TestCase):
         self.assertEqual(c.count(), 0)
         c.increment()
         self.assertEqual(c.count(), 1)
+
+    def test_increment_negative(self):
+        c = Counter(CounterTest.tid)
+        c.increment(-1)
+        self.assertEqual(c.count(), 0)
+
+    def test_measure(self):
+        c = Counter(CounterTest.tid)
+        c.increment()
+        ms = c._measure()
+        self.assertEqual(len(ms), 1)
+        self.assertEqual(ms[CounterTest.tid.with_stat('count')], 1)
+        self.assertEquals(c.count(), 0)
