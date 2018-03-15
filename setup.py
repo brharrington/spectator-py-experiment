737df8d40c154d00a7b0bdd94d7377faf2ae70ef
Brian Harrington <brharrington@gmail.com>
2018-03-14 19:36:31 -0700

    update setup.py and and tox config

--- setup.py
+++ setup.py
@@ -11,4 +11,27 @@ setup(
     license='Apache 2.0',
     url='https://github.com/brharrington/spectator-py/',
     packages=['netflix', 'netflix.spectator'],
+    python_requires='>=2.7, <4',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+    
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+    
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: Apache Software License',
+    
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
 )
