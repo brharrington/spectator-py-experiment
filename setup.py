1b78e179703511daba74f4153995c484869a08da
Brian Harrington <brharrington@gmail.com>
2018-03-14 17:10:16 -0700

    update user email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.1',
     description='Python library for reporting metrics to Atlas.',
     author='Brian Harrington',
-    author_email='brharrington@netflix.com',
+    author_email='netflix-atlas@googlegroups.com',
     license='Apache 2.0',
     url='https://github.com/brharrington/spectator-py/',
     packages=['netflix', 'netflix.spectator'],
